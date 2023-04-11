af673ffb440a9d3ec0f298dc83e0923f744ea2ba
Craig Riley <c.riley@dsmdesign.co.uk>
2023-04-11 15:08:32 +0100

    Stripped down repo

--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  printWidth: 120,
-  semi: false,
-  singleQuote: true,
-  tabWidth: 2,
-  trailingComma: 'all',
-  pluginSearchDirs: false,
-  plugins: ['prettier-plugin-svelte', 'prettier-plugin-organize-imports', 'prettier-plugin-tailwindcss'],
-}
